72fd931c6b31d8514c811b7a349103daa8322c37
Mark Kaarma <mark.kaarma@gmail.com>
2021-10-18 10:10:46 +0300

    New code for FizzBuzz Test

---
+++ src/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,43 @@
+package fizzbuzz;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void of_zerois0() {
+		assertEquals(FizzBuzz.of(0), ("0"));
+	}
+
+	@Test
+	public void of_oneis1() {
+		assertEquals(FizzBuzz.of(1), ("1"));
+	}
+
+	@Test
+	public void of_threeisFizz() {
+	assertEquals(FizzBuzz.of(3), ("Fizz"));
+	}
+	
+	@Test
+	public void of_fiveisBuzz() {
+		assertEquals(FizzBuzz.of(5), ("Buzz"));
+	}
+	
+	@Test
+	public void of_sixisFizz() {
+		assertEquals(FizzBuzz.of(6), ("Fizz"));
+	}
+
+	@Test
+	public void of_tenisBuzz() {
+		assertEquals(FizzBuzz.of(10), ("Buzz"));
+	}
+
+	@Test
+	public void of_fiveteenisFizzBuzz() {
+		assertEquals(FizzBuzz.of(15), ("FizzBuzz"));
+	}
+}
