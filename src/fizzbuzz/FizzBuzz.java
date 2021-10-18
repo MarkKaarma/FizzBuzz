d3cdffe1e370229e2b5dcca5988f23e713784a6a
Mark Kaarma <mark.kaarma@gmail.com>
2021-10-18 19:12:12 +0300

    Added new comment in FizzBuzz

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -19,15 +19,15 @@ public class FizzBuzz
 
 	private static String _of(int i) {
 
-			String s = "";
+			String s = ""; 
 
 			if (i % 3 == 0)
 
-				s += "Fizz";
+				s += "Fizz"; // For multiples of three print Fizz instead of the number
 
 			if (i % 5 == 0)
 
-				s += "Buzz";
+				s += "Buzz"; // For the multiples of five print Buzz instead of the number
 
 			return !s.isEmpty() ? s : String.valueOf(i); // When in line is not empty result will give in console(Fizz,Buzz and etc)
 
